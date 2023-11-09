30e225d9f0713209e2e5d6f68636cb84f263ffef
voxquoocshuyy <voquochuy1502@gmail.com>
2023-11-09 16:42:07 +0700

    Update vnpay payment

--- src/WSS.API/Infrastructure/Services/VnPay/VnPayPaymentService.cs
+++ src/WSS.API/Infrastructure/Services/VnPay/VnPayPaymentService.cs
@@ -1,5 +1,8 @@
 using WSS.API.Application.Models.Requests;
+using WSS.API.Data.Repositories.Order;
+using WSS.API.Data.Repositories.PaymentHistory;
 using WSS.API.Infrastructure.Config;
+using WSS.API.Infrastructure.Utilities;
 using Task = System.Threading.Tasks.Task;
 
 namespace WSS.API.Infrastructure.Services.VnPay;
@@ -9,6 +12,8 @@ public class VnPayPaymentService : IVnPayPaymentService
     private readonly VnPaySettings _vnPaySettings;
 
     private readonly IHttpContextAccessor _contextAccessor;
+    private readonly IPaymentHistoryRepo _paymentHistoryRepo;
+    private readonly IOrderRepo _orderRepo;
 
     private const string PayCommand = "pay";
 
@@ -18,10 +23,13 @@ public class VnPayPaymentService : IVnPayPaymentService
 
     private const string DefaultPaymentInfo = "Thanh toán với VnPay";
 
-    public VnPayPaymentService(VnPaySettings vnPaySettings, IHttpContextAccessor contextAccessor)
+    public VnPayPaymentService(VnPaySettings vnPaySettings, IHttpContextAccessor contextAccessor,
+        IPaymentHistoryRepo paymentHistoryRepo, IOrderRepo orderRepo)
     {
         _vnPaySettings = vnPaySettings;
         _contextAccessor = contextAccessor;
+        _paymentHistoryRepo = paymentHistoryRepo;
+        _orderRepo = orderRepo;
     }
 
     public async Task<PaymentResponse> CreatePayment(VnPayPayment payment)
@@ -33,6 +41,9 @@ public class VnPayPaymentService : IVnPayPaymentService
         }
 
         var pay = new VnPayLibrary();
+        var orderInDb = await _orderRepo.GetOrderById(payment.OrderReferenceId);
+        if (orderInDb == null)
+            throw new Exception("Order not found");
         var urlCallBack = $"{_vnPaySettings.CallbackUrl}/{payment.OrderReferenceId}";
 
         pay.AddRequestData("vnp_Version", _vnPaySettings.Version);
@@ -46,7 +57,7 @@ public class VnPayPaymentService : IVnPayPaymentService
         pay.AddRequestData("vnp_OrderInfo", DefaultPaymentInfo);
         pay.AddRequestData("vnp_OrderType", payment.OrderType.ToString());
         pay.AddRequestData("vnp_ReturnUrl", urlCallBack);
-        pay.AddRequestData("vnp_TxnRef", DateTime.UtcNow.Ticks.ToString());
+        pay.AddRequestData("vnp_TxnRef", payment.OrderReferenceId.ToString());
         pay.AddRequestData("vnp_BankCode", string.Empty);
 
         var paymentUrl =
@@ -65,53 +76,59 @@ public class VnPayPaymentService : IVnPayPaymentService
     public async Task<PaymentResponse> Confirm()
     {
         HttpContext? context = _contextAccessor.HttpContext;
-        string returnUrl = _vnPaySettings.CallbackUrl;
-        float amount = 0;
-        string status = "failed";
         var response = new PaymentResponse();
-        if (context.Request.Query.Count > 0)
+        if (context!.Request.Query.Count > 0)
         {
-            string vnp_HashSecret = _vnPaySettings.HashSecret;
+            string vnpHashSecret = _vnPaySettings.HashSecret;
             var vnpayData = context.Request.Query;
             VnPayLibrary vnpay = new VnPayLibrary();
             foreach (string s in vnpayData.Keys)
             {
-                //get all querystring data
                 if (!string.IsNullOrEmpty(s) && s.StartsWith("vnp_"))
                 {
                     vnpay.AddResponseData(s, vnpayData[s]);
                 }
             }
-            //Lay danh sach tham so tra ve tu VNPAY
-            //vnp_TxnRef: Ma don hang merchant gui VNPAY tai command=pay    
-            //vnp_TransactionNo: Ma GD tai he thong VNPAY
-            //vnp_ResponseCode:Response code from VNPAY: 00: Thanh cong, Khac 00: Xem tai lieu
-            //vnp_SecureHash: HmacSHA512 cua du lieu tra ve
 
-            long orderId = Convert.ToInt64(vnpay.GetResponseData("vnp_TxnRef"));
-            float vnp_Amount = Convert.ToInt64(vnpay.GetResponseData("vnp_Amount")) / 100;
-            amount = vnp_Amount;
-            long vnpayTranId = Convert.ToInt64(vnpay.GetResponseData("vnp_TransactionNo"));
-            string vnp_ResponseCode = vnpay.GetResponseData("vnp_ResponseCode");
-            string vnp_TransactionStatus = vnpay.GetResponseData("vnp_TransactionStatus");
-            String vnp_SecureHash = context.Request.Query["vnp_SecureHash"];
-            bool checkSignature = vnpay.ValidateSignature(vnp_SecureHash, vnp_HashSecret);
+            var orderId = Convert.ToString(vnpay.GetResponseData("vnp_TxnRef"));
+            var totalAmount = Convert.ToInt64(vnpay.GetResponseData("vnp_Amount")) / 100;
+            string vnpResponseCode = vnpay.GetResponseData("vnp_ResponseCode");
+            string vnpTransactionStatus = vnpay.GetResponseData("vnp_TransactionStatus");
+            string orderType = vnpay.GetResponseData("vnp_OrderType");
+            String vnpSecureHash = context.Request.Query["vnp_SecureHash"];
+            bool checkSignature = vnpay.ValidateSignature(vnpSecureHash, vnpHashSecret);
             var vnp_OrderInfo = vnpay.GetResponseData("vnp_OrderInfo");
-            Guid companyId = Guid.Parse(vnp_OrderInfo);
-            //Cap nhat ket qua GD
-            //Yeu cau: Truy van vao CSDL cua  system => lay ra duoc Wallet
-            //get from DB
-            
-                if (vnp_ResponseCode == "00" && vnp_TransactionStatus == "00")
+
+            if (vnpResponseCode == "00" && vnpTransactionStatus == "00")
+            {
+                response.OrderType = OrderType.Deposit;
+                response.Status = PaymentStatus.Success;
+                
+                var order = await _orderRepo.GetOrderById(Guid.Parse(orderId));
+                if (order == null) throw new Exception("Order not found");
+                order.StatusOrder = (int)StatusOrder.DONE;
+                order.StatusPayment = (int)StatusPayment.DONE;
+                await _orderRepo.UpdateOrder(order);
+                
+                var code = await _paymentHistoryRepo.GetPaymentHistorys().OrderByDescending(x => x.Code)
+                    .Select(x => x.Code)
+                    .FirstOrDefaultAsync();
+                var paymentHistory = new PaymentHistory()
                 {
-                    response.OrderType = OrderType.Deposit;
-                    response.Status = PaymentStatus.Success;
-                }
-                else
-                {
-                    response.OrderType = OrderType.Deposit;
-                    response.Status = PaymentStatus.Failed;
-                }
+                    Id = Guid.NewGuid(),
+                    OrderId = Guid.Parse(orderId),
+                    TotalAmount = totalAmount,
+                    CreateDate = DateTime.UtcNow,
+                    PaymentType = orderType,
+                    Code = GenCode.NextId(code)
+                };
+                await _paymentHistoryRepo.CreatePaymentHistory(paymentHistory);
+            }
+            else
+            {
+                response.OrderType = OrderType.Deposit;
+                response.Status = PaymentStatus.Failed;
+            }
         }
 
         return await Task.FromResult(response);
