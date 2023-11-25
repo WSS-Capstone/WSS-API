3edda34b634e2db85756dc96251aed3978ac185c
HL <forest.tl112@gmail.com>
2023-11-26 04:24:59 +0700

    update response obj patch status combo

--- src/WSS.API/Application/Commands/Combo/UpdateComboStatusCommand.cs
+++ src/WSS.API/Application/Commands/Combo/UpdateComboStatusCommand.cs
@@ -2,13 +2,13 @@
 
 namespace WSS.API.Application.Commands.Combo;
 
-public class UpdateComboStatusCommand : IRequest<ComboServicesResponse>
+public class UpdateComboStatusCommand : IRequest<ComboResponse>
 {
     public Guid Id { get; set; }
     public ComboStatus Status { get; set; }
 }
 
-public class UpdateComboStatusCommandHandler : IRequestHandler<UpdateComboStatusCommand, ComboServicesResponse>
+public class UpdateComboStatusCommandHandler : IRequestHandler<UpdateComboStatusCommand, ComboResponse>
 {
     private readonly IMapper _mapper;
     private readonly IComboRepo _comboRepo;
@@ -19,7 +19,7 @@ public class UpdateComboStatusCommandHandler : IRequestHandler<UpdateComboStatus
         _comboRepo = comboRepo;
     }
 
-    public async Task<ComboServicesResponse> Handle(UpdateComboStatusCommand request,
+    public async Task<ComboResponse> Handle(UpdateComboStatusCommand request,
         CancellationToken cancellationToken)
     {
         var service = await _comboRepo.GetComboById(request.Id);
@@ -31,6 +31,6 @@ public class UpdateComboStatusCommandHandler : IRequestHandler<UpdateComboStatus
         service.Status = (int)request.Status;
         service.CreateDate = DateTime.Now;
         await _comboRepo.UpdateCombo(service);
-        return _mapper.Map<ComboServicesResponse>(service);
+        return _mapper.Map<ComboResponse>(service);
     }
 }
